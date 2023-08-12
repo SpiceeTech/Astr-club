180eb1fd9d6cedf4303f19751ca9c9535a0c4388
Nebula-sta <aishamkhize45@gmail.com>
2023-08-12 21:26:03 +0200

    with php...

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 //HEADER
 function imgSlider(anything){
-  document.querySelector('.images').src = anything;
-  }
-  function changeCircleColor(color){
-    const circle = document.querySelector('.circle');
-    circle.style.background = color;
+  document.querySelector('.astronaut').src = anything;
   }
 
   window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
   });
+
+  function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+  }
+
+  // AOS ANIMATION
+ /*  AOS.init({
+    duration: 1100,
+    once: true,
+  }); */
+
+
+
